660d40496d0f3ea412fa3620459fef89e5c86e8f
Ronica Singh <ladyinblack2010rs@gmail.com>
2026-06-10 11:17:00 +0200

    Calculate area of a circle using diameter
    Removed color variable and added calculations for area of a circle.

--- hello-python/exercise01.py
+++ hello-python/exercise01.py
@@ -1,5 +1,10 @@
-# create a variable called color with an appropriate value on the line below 
-# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
-color = "blue"
+pi = 3.14159    # approximate
+diameter = 3
 
-print(color)
+# Create a variable called 'radius' equal to half the diameter 
+radius = diameter * 0.5
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * radius ** 2
+
+print(area)
