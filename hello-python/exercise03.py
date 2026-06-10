89d318381a96d544464bf6ddd215ad9a35038401
Ronica Singh <ladyinblack2010rs@gmail.com>
2026-06-10 11:25:47 +0200

    Add exercises with parentheses for evaluation

---
+++ hello-python/exercise03.py
@@ -0,0 +1,8 @@
+# A. Add parentheses to the following expression so that it evaluates to 1
+# 5 - 3 // 2
+print((5 - 3) // 2)
+
+# B. Add parentheses to the following expression so that it evaluates to 0.
+# 8 - 3 * 2 - 1 + 1 
+# This is one solution, there can be others.
+print(8 - 3 * 2 - (1 + 1))
