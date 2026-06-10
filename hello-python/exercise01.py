c676e4125d0a080ba03e6dde403c71a498d45aed
Ronica Singh <ladyinblack2010rs@gmail.com>
2026-06-10 11:13:46 +0200

    Add color variable and print statement

---
+++ hello-python/exercise01.py
@@ -0,0 +1,5 @@
+# create a variable called color with an appropriate value on the line below 
+# (Remember, strings in Python must be enclosed in 'single' or "double" quotes)
+color = "blue"
+
+print(color)
